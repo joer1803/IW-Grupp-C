aae81ec0e102d41645bb26938f6d5374b628bb1c
Ida Gärdqvist <ida19891@hotmail.com>
2019-10-25 12:34:27 +0200

    Signup page for freelance and customer

--- HampesFrilansare/Controllers/HomeController.cs
+++ HampesFrilansare/Controllers/HomeController.cs
@@ -26,5 +26,19 @@ namespace HampesFrilansare.Controllers
 
             return View();
         }
+
+        public ActionResult SignupFreelance()
+        {
+            ViewBag.Message = "Your freelance sign up page.";
+
+            return View();
+        }
+
+        public ActionResult SignupCustomer()
+        {
+            ViewBag.Message = "Your customer sign up page.";
+
+            return View();
+        }
     }
 }
