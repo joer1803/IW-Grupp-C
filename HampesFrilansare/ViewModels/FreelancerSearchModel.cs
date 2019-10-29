df2e202422caa5bc8bb2a4820ea36ee934104787
joer1803 <joer1803@student.miun.se>
2019-10-29 15:40:55 +0100

    gjort en viewmodel och en searchcontroller

---
+++ HampesFrilansare/ViewModels/FreelancerSearchModel.cs
@@ -0,0 +1,24 @@
+﻿using HampesFrilansare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HampesFrilansare.ViewModels
+{
+    public class FreelancerSearchModel
+    {
+        public int freelancerID { get; set; }
+        public int skillID { get; set; }
+
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+
+        public virtual Competence Competence { get; set; }
+        public Nullable<int> rating { get; set; }
+        public string name { get; set; }
+
+
+
+    }
+}
