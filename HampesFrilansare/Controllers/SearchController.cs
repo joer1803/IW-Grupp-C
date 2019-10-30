d1e1cc154bec0b17b91f33bcd7f07b1980d2dc76
joer1803 <joer1803@student.miun.se>
2019-10-30 18:44:00 +0100

    massa skit

--- HampesFrilansare/Controllers/SearchController.cs
+++ HampesFrilansare/Controllers/SearchController.cs
@@ -12,7 +12,19 @@ namespace HampesFrilansare.Controllers
     {
         hampesfrilansdbEntities db = new hampesfrilansdbEntities();
 
-        // GET: Search
+        public ActionResult SearchFreelancer(string searchstr)
+        {
+            if (searchstr != null)
+            {
+                var free = GetFreelancerVM().Where(f => (f.firstname.Contains(searchstr)) || (f.lastname.Contains(searchstr)) || (f.compname.Contains(searchstr)) || (f.skillname.Contains(searchstr))).ToList();
+                return View(free);
+            }
+            else
+            {
+                return View(GetFreelancerVM());
+            }
+        }
+
         public List<FreelancerSearchModel> GetFreelancerVM()
         {
             var sfree = (from free in db.Freelancer
@@ -22,24 +34,23 @@ namespace HampesFrilansare.Controllers
                          equals comp.resumeID
                          join skill in db.Skill on comp.competenceID
                          equals skill.competenceID
-                         select new { free.firstname, free.lastname, comp.name, skillname = skill.name });
+                         select new { free.freelancerID, free.firstname, free.lastname, comp.name, skillname = skill.name, comp.category });
+
             List<FreelancerSearchModel> freeVM = new List<FreelancerSearchModel>();
             foreach (var f in sfree)
             {
                 FreelancerSearchModel model = new FreelancerSearchModel();
+                model.freelancerID = f.freelancerID;
                 model.firstname = f.firstname;
                 model.lastname = f.lastname;
-                model.compname.Add(f.name);
-                model.skillname.Add(f.skillname);
+                model.compname= f.name;
+                model.skillname = f.skillname;
+                model.compcategory = f.category;
+
                 freeVM.Add(model);
             }
 
             return freeVM;
-        }
-        public ViewResult SearchFreelancers(string searchstr)
-        {
-            var free = GetFreelancerVM().Where(f => (f.firstname.Contains(searchstr)) || (f.lastname.Contains(searchstr)) || (f.compname.Contains(searchstr)) || (f.skillname.Contains(searchstr))).ToList();
-            return View(free);
         }
     }
 }
--- HampesFrilansare/ViewModels/FreelancerSearchModel.cs
+++ HampesFrilansare/ViewModels/FreelancerSearchModel.cs
@@ -8,21 +8,15 @@ namespace HampesFrilansare.ViewModels
 {
     public class FreelancerSearchModel
     {
-        public FreelancerSearchModel()
-        {
-            skillname = new List<string>();
-            compname = new List<string>();
-        }
         public int freelancerID { get; set; }
-        public int skillID { get; set; }
-
         public string firstname { get; set; }
         public string lastname { get; set; }
         public string fullname => $"{firstname} {lastname}";
 
         public Nullable<int> rating { get; set; }
-        public List<string> skillname { get; set; }
-        public List<string> compname { get; set; }
+        public string skillname { get; set; }
+        public string compname { get; set; }
+        public string compcategory { get; set; }
 
 
 
