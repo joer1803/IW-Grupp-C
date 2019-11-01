09d0ef228d7d546f55858cfc33006c01e55262db
Ida Gärdqvist <ida19891@hotmail.com>
2019-11-01 16:52:20 +0100

    Viewmodel

---
+++ HampesFrilansare/ViewModels/FreelancerSearchModelA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HampesFrilansare.Models;
+
+namespace HampesFrilansare.ViewModels
+{
+    public class FreelancerSearchModelA
+    {
+        private List<string> Categories()
+        {
+            List<string> cats = new List<string>();
+            var categories = (from comps in db.Competence
+                              select new { category = comps.category });
+            return cats = categories.Distinct().Select(x => x.category.ToString()).ToList();
+        }
+    }
+}
