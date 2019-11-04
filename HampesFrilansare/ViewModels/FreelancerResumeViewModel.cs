cede85c6c58368f6526877fc595a7ad7a9cc9a01
Preifelt <hapr1800@student.miun.se>
2019-11-04 12:40:45 +0100

    lagt till freelancerResume lagt till boxar för Freelancer/create

---
+++ HampesFrilansare/ViewModels/FreelancerResumeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HampesFrilansare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HampesFrilansare.ViewModels
+{
+    public class FreelancerResumeViewModel
+    {
+        public Freelancer freelancer { get; set; }
+        public Resume resume { get; set; }
+        public Education education { get; set; }
+        public Driverslicence driverslicence { get; set; }
+        public Experience experience { get; set; }
+        public Skill skill { get; set; }
+        public Competence competence { get; set; }
+        public Language language { get; set; }
+
+    }
+}
